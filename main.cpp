eb5cb72088f876a9a5434f3bf2485eefc328f2ea
ignascerniauskas <145258320+ignascerniauskas@users.noreply.github.com>
2023-10-12 14:52:25 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,29 +3,14 @@
 int main() {
 
     srand(static_cast<unsigned>(time(0)));
-    vector<studentas> grupe;
+    vector<studentas> grupe1000, grupe10000, grupe100000, grupe1000000, grupe10000000;
     
-    char arSkaityti;
-    cout<<"Ar norite nuskaityti duomenis is failo? (T/N): ";//Pasirenkame, ar norime skaityti duomenis is failo
-    cin>>arSkaityti;
-    if(arSkaityti=='T' || arSkaityti=='t'){
-        string failoPavadinimas;
-        cout<<"Iveskite failo pavadinima: ";
-        cin>>failoPavadinimas;
-        skaitytiFaila(failoPavadinimas,grupe); //nuskaitome duomenis is failo
-        sort(grupe.begin(), grupe.end(), rikiavimas);//surikiuojame
-        isvedimasFaile(grupe); //irasome rezultatus i faila 'rez.txt'
-    }
-    else{
-       cout<<'\n';
-       duomenuIvedimas(grupe); //jei nenorime skaityti duomenu is failo duomenis ivedime
-       char pasirinkimas;
-       cout << "Jei norite naudoti vidurki, spauskite 'V' arba 'v', jei norite naudoti mediana spauskite 'M' arba 'm': ";
-       cin >> pasirinkimas;
-       bool naudotividurki = (pasirinkimas=='V' || pasirinkimas=='v');
-       rezultatuIsvendimasEkrane(grupe, naudotividurki);
+    matuotiLaika("studentas.txt", grupe1000, 1000);
+    matuotiLaika("studentas.txt", grupe10000, 10000);
+    matuotiLaika("studentas.txt", grupe100000, 100000);
+    matuotiLaika("studentas.txt", grupe1000000, 1000000);
+    matuotiLaika("studentas.txt", grupe10000000, 10000000);
 
-    }
 
     return 0;
 }
