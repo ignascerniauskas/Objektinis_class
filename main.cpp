09dae61f439e70ab74c429342dada60b51a838b7
ignascerniauskas <145258320+ignascerniauskas@users.noreply.github.com>
2023-10-14 18:23:01 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,14 @@ int main() {
 
     srand(static_cast<unsigned>(time(0)));
     vector<studentas> grupe1000, grupe10000, grupe100000, grupe1000000, grupe10000000;
-    
-    matuotiLaika("studentas.txt", grupe1000, 1000);
-    matuotiLaika("studentas.txt", grupe10000, 10000);
-    matuotiLaika("studentas.txt", grupe100000, 100000);
-    matuotiLaika("studentas.txt", grupe1000000, 1000000);
-    matuotiLaika("studentas.txt", grupe10000000, 10000000);
+    vector<studentas> moksliukai;
+    vector<studentas> varksiukai;
 
+    matuotiLaika("studentai.txt",grupe1000,1000,moksliukai,varksiukai);
+    matuotiLaika("studentai.txt",grupe10000,10000,moksliukai, varksiukai);
+    matuotiLaika("studentai.txt",grupe100000,100000,moksliukai,varksiukai);
+    matuotiLaika("studentai.txt",grupe1000000,1000000,moksliukai,varksiukai);
+    matuotiLaika("studentai.txt",grupe10000000,10000000,moksliukai,varksiukai);
 
     return 0;
 }
