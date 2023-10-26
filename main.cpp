d54947abcf1de605cf351ab9082383b353648099
ignascerniauskas <145258320+ignascerniauskas@users.noreply.github.com>
2023-10-26 18:58:29 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,13 +14,16 @@ int main() {
     if(arMatuoti=='T' || arMatuoti=='t'){
     cout<<"Pasirinkite rusiavimo tvarka: (Vardas-V/v, Pavarde-P/p, Galutinis balas- G/g)"<<endl;
     char pasirinkimas;
+    char generavimas;
     cin>>pasirinkimas;
+    cout<<"Ar norite generuoti atsitiktinius rezultatus (ar naudoti jau esancius) (T/N): "<<endl;
+    cin>>generavimas;
 
-    matuotiLaika("studentai.txt",grupe1000,1000,moksliukai,varksiukai,pasirinkimas);
-    matuotiLaika("studentai.txt",grupe10000,10000,moksliukai, varksiukai, pasirinkimas);
-    matuotiLaika("studentai.txt",grupe100000,100000,moksliukai,varksiukai, pasirinkimas);
-    matuotiLaika("studentai.txt",grupe1000000,1000000,moksliukai,varksiukai, pasirinkimas);
-    matuotiLaika("studentai.txt",grupe10000000,10000000,moksliukai,varksiukai, pasirinkimas);
+    matuotiLaika("studentai.txt",grupe1000,1000,moksliukai,varksiukai,pasirinkimas,generavimas);
+    matuotiLaika("studentai.txt",grupe10000,10000,moksliukai, varksiukai, pasirinkimas,generavimas);
+    matuotiLaika("studentai.txt",grupe100000,100000,moksliukai,varksiukai, pasirinkimas,generavimas);
+    matuotiLaika("studentai.txt",grupe1000000,1000000,moksliukai,varksiukai, pasirinkimas,generavimas);
+    matuotiLaika("studentai.txt",grupe10000000,10000000,moksliukai,varksiukai, pasirinkimas,generavimas);
     }
     else{
        cout<<'\n';
