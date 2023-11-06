817c4fed5cf2ce6ce006d8a93b8d4e289d9109c5
ignascerniauskas <145258320+ignascerniauskas@users.noreply.github.com>
2023-11-06 20:03:51 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,21 +1,16 @@
-#include "my_lib.h";
+#include "myLib.h";
 
 int main() {
 
     srand(static_cast<unsigned>(time(0)));
-    list<studentas> grupe;
-    list<studentas> grupe1000, grupe10000, grupe100000, grupe1000000, grupe10000000;
-    list<studentas> moksliukai, varksiukai;
+    vector<studentas> grupe1000, grupe10000, grupe100000, grupe1000000, grupe10000000;
+    vector<studentas> moksliukai;
+    vector<studentas> varksiukai;
 
-
-    char arMatuoti;
-    cout<<"Ar norite matuti laika? (T/N): ";
-    cin>>arMatuoti;
-    if(arMatuoti=='T' || arMatuoti=='t'){
     cout<<"Pasirinkite rusiavimo tvarka: (Vardas-V/v, Pavarde-P/p, Galutinis balas- G/g)"<<endl;
     char pasirinkimas, generavimas, strategija;
     cin>>pasirinkimas;
-    cout<<"Pasirinkite strategija (1,2 arba 3)"<<endl;
+    cout<<"Pasirinkite strategija (1, 2 arba 3)"<<endl;
     cin>>strategija;
     cout<<"Ar norite generuoti atsitiktinius rezultatus (ar naudoti jau esancius) (T/N): "<<endl;
     cin>>generavimas;
@@ -25,16 +20,8 @@ int main() {
     matuotiLaika("studentai.txt",grupe100000,100000,moksliukai,varksiukai, pasirinkimas,generavimas,strategija);
     matuotiLaika("studentai.txt",grupe1000000,1000000,moksliukai,varksiukai, pasirinkimas,generavimas,strategija);
     matuotiLaika("studentai.txt",grupe10000000,10000000,moksliukai,varksiukai, pasirinkimas,generavimas,strategija);
-    }
-    else{
-       cout<<'\n';
-       duomenuIvedimas(grupe);
-       char pasirinkimas;
-       cout << "Jei norite naudoti vidurki, spauskite 'V' arba 'v', jei norite naudoti mediana spauskite 'M' arba 'm': ";
-       cin >> pasirinkimas;
-       bool naudotividurki = (pasirinkimas=='V' || pasirinkimas=='v');
-       rezultatuIsvendimasEkrane(grupe, naudotividurki);
-    }
+
 
     return 0;
 }
+
