cdf2482281ce84fe3605a2c6dc0fdbe20360b361
ignascerniauskas <145258320+ignascerniauskas@users.noreply.github.com>
2023-11-25 12:04:37 +0200

    Update my_lib.h

--- List/my_lib.h
+++ List/my_lib.h
@@ -35,24 +35,27 @@ struct studentas {
   float medGalutinis;
 };
 
-void duomenuIvedimas(list<studentas>& grupe);
-void apskaiciuotiVidurki(studentas& stud);
-void apskaiciuotiMediana(studentas& stud);
-void rezultatuIsvendimasEkrane(list<studentas>& grupe, bool naudotividurki);
-void generuotiPazymius(studentas& stud);
-void generuotiPazymius1(studentas& stud, int nd_sk);
-void generuotiEgzamina(studentas& stud);
-void generuotiStudentuSarasa(list<studentas>& grupe, int studentuSkaicius);
-void skaitytiFaila(const string& failopav, list<studentas>& grupe);
-void isvedimasFaile (list<studentas>& grupe, const string& failoPavadinimas);
+void duomenuIvedimas(list<studentas>& grupe);//
+void apskaiciuotiVidurki(studentas& stud);//
+void apskaiciuotiMediana(studentas& stud);//
+void rezultatuIsvendimasEkrane(list<studentas>& grupe, bool naudotividurki);//
+void generuotiPazymius(studentas& stud);//
+void generuotiPazymius1(studentas& stud, int nd_sk);//
+void generuotiEgzamina(studentas& stud);//
+void generuotiStudentuSarasa(list<studentas>& grupe, int studentuSkaicius);//
+void skaitytiFaila(const string& failopav, list<studentas>& grupe);//
+void isvedimasFaile (list<studentas>& grupe, const string& failoPavadinimas);//
 
-bool palyginti(const string& a, const string& b);
+bool palyginti(const string& a, const string& b);//
 
-void irasytiIFaila(const list<studentas>& grupe, const string& failoPavadinimas);
-void suskirstymas1(list<studentas>& grupe, list<studentas>& moksliukai, list<studentas>& varksiukai);
-void suskirstymas2(list<studentas>& grupe, list<studentas>& varksiukai);
-void suskirstymas3(list<studentas>& grupe, list<studentas>& varksiukai);
+void irasytiIFaila(const list<studentas>& grupe, const string& failoPavadinimas);//
+void suskirstymas(list<studentas>& grupe, list<studentas>& varksiukai);//
 
-void matuotiLaika(const string& failoPavadinimas, list<studentas>& grupe, int stud_skaicius,list<studentas>& moksliukai, list<studentas>& varksiukai, char pasirinkimas, char generavimas, char startegija);
+void matuotiLaika(const string& failoPavadinimas, list<studentas>& grupe, int stud_skaicius,list<studentas>& moksliukai, list<studentas>& varksiukai, char pasirinkimas, char generavimas);//
+
+
+
+
+
 
 
