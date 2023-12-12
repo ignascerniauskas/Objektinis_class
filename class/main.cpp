126ee22729c1cb26523d04cd5fbe6ffa3db848e3
ignascerniauskas <145258320+ignascerniauskas@users.noreply.github.com>
2023-12-12 18:14:56 +0200

    Update main.cpp

--- class/main.cpp
+++ class/main.cpp
@@ -2,35 +2,7 @@
 
 
 int main() {
-
-    //zmogus jonas;   //negalima sugurti objekto is klases zmogus
-
-    studentas s;
-
-    s.setVard("Jonas");
-    s.setPav("Jonaitis");
-    s.displayInfo();
-    cout<<"-------------------"<<endl;
-
-    studentas obj1;
-    obj1.setVard("Antanas");
-    obj1.setPav("Antanaitis");
-    obj1.addPazymys(2);
-    obj1.addPazymys(3);
-    obj1.addPazymys(5);
-    obj1.addPazymys(1);
-    obj1.setEgzaminas(6);
-
-    studentas obj2 = obj1;
-    studentas obj3;
-    obj3 = obj1;
-
-    cout<<"Informacija apie obj1: "<<obj1<<endl;
-    cout<<"Informacija apie obj2: "<<obj2<<endl;
-    obj3.displayInfo();
-
-    //----------------------
-
+    
     srand(static_cast<unsigned>(time(0)));
     list<studentas> grupe;
     list<studentas> grupe1000, grupe10000, grupe100000, grupe1000000, grupe10000000;
